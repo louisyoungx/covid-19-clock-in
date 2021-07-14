009ce5fce8c9fc6c13f3b0fe063b3408f201ff65
Louis Young <1462648167>
2021-07-15 06:32:05 +0800

    <init>(*): initialize

---
+++ message.py
@@ -0,0 +1,63 @@
+import json
+import requests
+
+def sendFriendMessage(message, userid):
+    try:
+        URL = "www.louisyoung.site:8088"
+        path = "sendFriendMessage"
+        URL = "http://{}/{}".format(URL, path)
+
+        body = {
+            "sessionKey": "YourSession",
+            "target": userid,
+            "messageChain": [
+                {"type": "Plain", "text": message}
+            ]
+        }
+
+        # sender = requests.post(URL, params = params, data=json.dumps(body))
+        sender = requests.post(URL, data=json.dumps(body))
+
+        mes = message.replace("\n", " ")
+        if len(mes) > 10:
+            mes = mes[:10] + "···"
+        print("Send {}".format(mes))
+
+
+        # print(sender.request.url)
+        # sender.raise_for_status()
+        # print(sender.text)
+        return True
+    except:
+        logger.error("Message Send Failed")
+        return False
+
+
+def sendGroupMessage(message, groupid):
+    try:
+        URL = "www.louisyoung.site:8088"
+        path = "sendGroupMessage"
+        URL = "http://{}/{}".format(URL, path)
+
+        body = {
+            "sessionKey": "YourSession",
+            "target": groupid,
+            "messageChain": [
+                {"type": "Plain", "text": message}
+            ]
+        }
+
+        # sender = requests.post(URL, params = params, data=json.dumps(body))
+        sender = requests.post(URL, data=json.dumps(body))
+        mes = message.replace("\n", " ")
+        if len(mes) > 10:
+            mes = mes[:10] + "···"
+        print("Send {}".format(mes))
+
+        # print(sender.request.url)
+        # sender.raise_for_status()
+        # print(sender.text)
+        return True
+    except:
+        logger.error("Message Send Failed")
+        return False
---
+++ runserver.py
@@ -0,0 +1,4 @@
+from batchClockIn import clockIn
+
+if __name__ == '__main__':
+    clockIn()
